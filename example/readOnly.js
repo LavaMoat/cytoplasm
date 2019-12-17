54934e463a7ffac6cb5a3495a9050e8de12741c8
kumavis <aaron@kumavis.me>
2019-12-17 15:33:51 +0800

    examples - fix readOnly example

--- example/readOnly.js
+++ example/readOnly.js
@@ -1,10 +1,12 @@
+"use strict"
+
 const { Membrane } = require('../src/index')
 const createReadOnlyDistortion = require('../src/distortions/readOnly')
 
 const membrane = new Membrane()
-const graphA = membrane.makeObjectGraph({ label: 'a' })
-const graphB = membrane.makeObjectGraph({ label: 'b' })
 const readOnlyDistortion = createReadOnlyDistortion()
+const graphA = membrane.makeObjectGraph({ label: 'a', handler: readOnlyDistortion })
+const graphB = membrane.makeObjectGraph({ label: 'b' })
 
 const objA = {
   value: 1,
@@ -13,4 +15,11 @@ const objA = {
   }
 }
 
-const readOnlyA = membrane.bridge(objA, graphA, graphB, readOnlyDistortion)
+const readOnlyA = membrane.bridge(objA, graphA, graphB)
+
+try {
+  readOnlyA.value = 2
+  console.log(readOnlyA.value)
+} catch (err) {
+  console.log('*Correctly* encountered error while trying to update value:', err)
+}
