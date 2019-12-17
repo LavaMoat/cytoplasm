ee0a62b67c202806c7c89f8a4416ce3ab935d4cd
kumavis <aaron@kumavis.me>
2019-12-17 15:39:59 +0800

    distortions/revokable - expose a revoke fn

--- src/distortions/revokable.js
+++ src/distortions/revokable.js
@@ -1,60 +1,70 @@
-module.exports = createReadOnlyDistortion
+module.exports = createRevokableDistortion
 
 // still allows functions that cause side effects
-function createReadOnlyDistortion () {
+function createRevokableDistortion () {
   let revoked = false
-  return {
+  const api = {
+    revoke: () => {
+      revoked = true
+    }
+  }
+  const handler = {
     getPrototypeOf: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.getPrototypeOf(...args)
     },
     setPrototypeOf: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.setPrototypeOf(...args)
     },
     isExtensible: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.isExtensible(...args)
     },
     preventExtensions: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.preventExtensions(...args)
     },
     getOwnPropertyDescriptor: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.getOwnPropertyDescriptor(...args)
     },
     defineProperty: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.defineProperty(...args)
     },
     has: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.has(...args)
     },
     get: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.get(...args)
     },
     set: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.set(...args)
     },
     deleteProperty: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.deleteProperty(...args)
     },
     ownKeys: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.ownKeys(...args)
     },
     apply: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.apply(...args)
     },
     construct: (...args) => {
-      if (revoked) throw new Error('membrane was revoked')
+      if (revoked) throw new Error('Cytoplasm RevokableDistortion was revoked')
       return Reflect.construct(...args)
     },
-  },
+  }
+
+  return {
+    api,
+    handler,
+  }
 }
