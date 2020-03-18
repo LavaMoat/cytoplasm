6d8716af5fe0065802563e51308a8069acb47bf9
kumavis <aaron@kumavis.me>
2020-03-18 11:01:03 +0800

    getIntrinsics - improve error message

--- src/getIntrinsics.js
+++ src/getIntrinsics.js
@@ -4,7 +4,8 @@ module.exports = {
     try {
       return getIntrinsics()
     } catch (err) {
-      throw new Error(`Cytoplasm failed to gather intrinsics. Please specify a "primordials" option to the Membrane constructor, apply core-js polyfills, or use node v12 or higher.\n${err.stack}`)
+      const subErrMsg = err.stack || err.message || err
+      throw new Error(`Cytoplasm failed to gather intrinsics. Please specify a "primordials" option to the Membrane constructor, apply core-js polyfills, or use node v12 or higher.\n${subErrMsg}`)
     }
   }
 }
